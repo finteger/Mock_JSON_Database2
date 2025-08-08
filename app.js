bfe9c0e40b309507f6d9cc506cce3d345596eaf5
finteger <nashassociatesinc@gmail.com>
2025-08-07 18:32:32 -0600

    Update

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+const mongoose = require('require');
+const PORT = process.env.PORT;
+const uri = process.env.MONGO_URI;
+
+
+
+mongoose.connect(uri).then(
+    async() =>{
+
+        console.log('Connected to MongoDB.')
+
+        app.listen(PORT, ()=>{
+            console.log(`Connected to port ${PORT}`);
+        });
+    }
+).catch((err) =>{ console.log(`Error: ${error}`)});
