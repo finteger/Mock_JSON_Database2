d79fb40d20f46a78ae8a3beecc6dc682b8f158d6
finteger <nashassociatesinc@gmail.com>
2025-08-19 18:25:44 -0600

    Update

--- app.js
+++ app.js
@@ -4,7 +4,20 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 const PORT = process.env.PORT;
 const uri = process.env.MONGO_URI;
+const ejs = require('ejs');
 
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+app.use(express.static('public'));
+
+//route handler for home page
+app.get('/home', (req, res) => {
+    res.render('home');
+});
 
 
 mongoose.connect(uri).then(
