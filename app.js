0042b16fbe5408fc899695d054974f994081aca5
finteger <nashassociatesinc@gmail.com>
2025-08-25 17:45:41 -0600

    Update

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const PORT = process.env.PORT;
 const uri = process.env.MONGO_URI;
 const ejs = require("ejs");
 const { readData, writeData } = require("./utils/file.js");
+const userRoutes = require('./routes/users.js');
 
 //setup the view engine
 app.set("view engine", "ejs");
@@ -14,21 +15,8 @@ app.set("views", "./views");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
+app.use(userRoutes);
 
-//route handler for home page
-app.get("/home", (req, res) => {
-  res.render("home");
-});
-
-//api endpoint for exposing the resource
-app.get("/api/v1/users", async (req, res) => {
-  try {
-    const data = await readData();
-    res.status(200).send(data);
-  } catch (error) {
-    res.status(500).send(`Internal Server Error: ${error.message}`);
-  }
-});
 
 mongoose
   .connect(uri)
@@ -40,5 +28,5 @@ mongoose
     });
   })
   .catch((err) => {
-    console.log(`Error: ${error}`);
+    console.log(`Error: ${err}`);
   });
---
+++ routes/users.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+
+
+//route handler for home page
+router.get("/home", (req, res) => {
+  res.ender("home");
+});
+
+//api endpoint for exposing the resource
+router.get("/api/v1/users", async (req, res) => {
+  try {
+    const data = await readData();
+    res.status(200).send(data);
+  } catch (error) {
+    res.status(500).send(`Internal Server Error: ${error.message}`);
+  }
+});
+
+
+module.exports = router;
