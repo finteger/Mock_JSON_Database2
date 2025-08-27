8d4cd1d83650b8883039d1b1e02b5cbd1928fd32
finteger <nashassociatesinc@gmail.com>
2025-08-27 17:23:10 -0600

    Update

--- app.js
+++ app.js
@@ -10,6 +10,14 @@ const userRoutes = require('./routes/users.js');
 const YAML = require('yamljs');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = YAML.load('./swagger.yaml');
+const rateLimit = require('express-rate-limit');
+
+
+const fixedRateLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 10,
+  message: "Too many requests, please try again later."
+});
 
 //setup the view engine
 app.set("view engine", "ejs");
@@ -18,8 +26,11 @@ app.set("views", "./views");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
+app.use(fixedRateLimiter);
 app.use(userRoutes);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+
 
 mongoose
   .connect(uri)
