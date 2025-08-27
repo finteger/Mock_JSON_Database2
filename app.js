7ffd751986736b8948847e619322e722206f41d7
finteger <nashassociatesinc@gmail.com>
2025-08-27 16:51:55 -0600

    Update

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const uri = process.env.MONGO_URI;
 const ejs = require("ejs");
 const { readData, writeData } = require("./utils/file.js");
 const userRoutes = require('./routes/users.js');
+const YAML = require('yamljs');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = YAML.load('./swagger.yaml');
 
 //setup the view engine
 app.set("view engine", "ejs");
@@ -16,7 +19,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 app.use(userRoutes);
-
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 mongoose
   .connect(uri)
